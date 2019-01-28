858baabd60c5f4e134a73821268774ec43c75fbf
Vilius Vystartas <vilius.vystartas@gmail.com>
2019-01-28 22:44:12 +0000

    Started work on React frontend

--- app/src/App.js
+++ app/src/App.js
@@ -3,22 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    isLoading: true,
+    owners: []
+  };
+
+  async componentDidMount() {
+    const response = await fetch('/api/owners');
+    const body = await response.json();
+    this.setState({ owners: body, isLoading: false });
+  }
+
   render() {
+    const {owners, isLoading} = this.state;
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="App-intro">
+            <h2>Owner list</h2>
+            {owners.map(owner =>
+                <div key={owner.id}>
+                  {owner.firstName}
+                </div>
+            )}
+          </div>
         </header>
       </div>
     );
